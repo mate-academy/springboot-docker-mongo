f18df5d3ecbcf1310b7a80befd279116d06e3a98
Bohdan Chupika <chupika.bohdan@ukr.net>
2021-07-14 13:20:32 +0300

    add projection example

--- src/main/java/mate/academy/bootmongodocker/repository/UserRepository.java
+++ src/main/java/mate/academy/bootmongodocker/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package mate.academy.bootmongodocker.repository;
 
+import java.util.List;
 import mate.academy.bootmongodocker.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface UserRepository extends MongoRepository<User, String> {
+    // Example of projection
+    @Query(value="{}", fields="{name : 1, _id : 0}")
+    List<User> findNameAndExcludeId();
 }
